223c8c31a7221eada6886fdf23fbbb22d1d4048a
Rickard Östman <rost@spotify.com>
2015-11-26 17:19:25 +0100

    dump() -> tostring()

--- pyIOSXR/iosxr.py
+++ pyIOSXR/iosxr.py
@@ -117,7 +117,7 @@ class IOSXR:
         Allow a user to query a device directly using XML-requests
         """
         result = __execute_rpc__(self.device, rpc_command, self.timeout)
-        return ET.dump(result)
+        return ET.tostring(result)
 
     def open(self):
         """
