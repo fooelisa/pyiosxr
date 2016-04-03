2c64f55ac7fadcb1d7c325220aef9a0d1cf32948
Elisa Jasinska <elisa@jasinska.de>
2016-04-02 22:14:15 -0700

    testing __init__()

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import mock
 import unittest
 
@@ -12,7 +13,6 @@ from pyIOSXR.exceptions import XMLCLIError, InvalidInputError, TimeoutError, EOF
 # def __execute_config_show__(device, show_command, timeout):
 
 # test class IOSXR
-#     def __init__(self, hostname, username, password, port=22, timeout=60, logfile=None, lock=True):
 #     def __getattr__(self, item):
 #     def make_rpc_call(self, rpc_command):
 #     def open(self):
@@ -27,3 +27,53 @@ from pyIOSXR.exceptions import XMLCLIError, InvalidInputError, TimeoutError, EOF
 #     def discard_config(self):
 #     def rollback(self):
 
+
+#     def __init__(self, hostname, username, password, port=22, timeout=60, logfile=None, lock=True):
+
+class Test__init__(unittest.TestCase):
+
+    def test_init(self):
+        '''
+        Test pyiosxr class init
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd'))
+
+    def test_init_no_lock(self):
+        '''
+        Test pyiosxr class init - woithout locking
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd', lock=False))
+
+    def test_init_log_stdout(self):
+        '''
+        Test pyiosxr class init - log to stdout
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd', logfile=sys.stdout))
+
+    def test_init_log_file(self):
+        '''
+        Test pyiosxr class init - log to file
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd', logfile='filename'))
+
+    def test_init_port(self):
+        '''
+        Test pyiosxr class init - pass port number
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd', port=22))
+
+    def test_init_timeout(self):
+        '''
+        Test pyiosxr class init - pass timeout
+        Should return True
+        '''
+        self.assertTrue(IOSXR(hostname='hostname', username='ejasinska', password='passwd', timeout=120))
+
+
+if __name__ == '__main__':
+    unittest.main()
