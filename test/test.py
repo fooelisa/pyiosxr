da7bf2446d1f022f643dd468fe5da3797511889b
Elisa Jasinska <elisa@jasinska.de>
2016-05-02 02:08:16 +0200

    adding some _execute_rpc_ tests

--- test/test.py
+++ test/test.py
@@ -39,9 +39,9 @@ class TestExecuteRpc(unittest.TestCase):
         Should return True
         '''
         def sendline(foo):
-            pass
-        def expect_exact(foo, timeout):
             raise TimeoutError('error')
+        def expect_exact(foo, timeout):
+            pass
         device = mock.Mock()
         setattr(device, 'sendline', sendline)
         setattr(device, 'expect_exact', expect_exact)
@@ -53,9 +53,9 @@ class TestExecuteRpc(unittest.TestCase):
         Should return True
         '''
         def sendline(foo):
-            pass
-        def expect_exact(foo, timeout):
             raise EOFError('error')
+        def expect_exact(foo, timeout):
+            pass
         device = mock.Mock()
         setattr(device, 'sendline', sendline)
         setattr(device, 'expect_exact', expect_exact)
